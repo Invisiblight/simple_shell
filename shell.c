39ebc79c2b476c5ef764fcb841bd228bbc979f7c
Invisiblight <lightningbrain321@gmail.com>
2023-05-24 10:53:43 -0700

    s

--- shell.c
+++ shell.c
@@ -1,53 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-void display_prompt(void)
-{
-    printf("#cisfun$ ");
-}
+#define MAX_COMMAND_LENGTH 100
 
-int main(void)
-{
-    char command[100];  // Buffer to store the user's command
-    int ret;            // Return value of the execve system call
+int main(void) {
+    char command[MAX_COMMAND_LENGTH];
+    int ret;
 
     while (1) {
-        display_prompt();  // Display the prompt
+        printf("#cisfun$ ");
+        fflush(stdout);
 
         if (fgets(command, sizeof(command), stdin) == NULL) {
-            // Handle the end of file condition (Ctrl+D)
             printf("\n");
             break;
         }
 
-        // Remove the newline character from the command
         command[strcspn(command, "\n")] = '\0';
 
-        // Fork a child process
-        pid_t pid = fork();
-
-        if (pid == -1) {
-            // Handle fork error
+        ret = fork();
+        if (ret == -1) {
             perror("fork");
             exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            // Child process
-            ret = execve(command, NULL, NULL);
+        } else if (ret == 0) {
+            ret = execve(command, (char *[]){command, NULL}, NULL);
             if (ret == -1) {
-                // Handle command not found error
-                perror(command);
+                perror("execve");
                 exit(EXIT_FAILURE);
             }
+            exit(EXIT_SUCCESS);
         } else {
-            // Parent process
-            wait(NULL);  // Wait for the child process to finish
+            wait(NULL);
         }
     }
 
     return 0;
 }
-
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 shell.c -o shell
-
-./shell
